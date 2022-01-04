a574839a9423a06fd5c456976cf6abdf38048c92
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-01-04 16:42:09 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/pauloricardo/crusomc/domain/Categoria.java
@@ -0,0 +1,53 @@
+package com.pauloricardo.crusomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable{
+
+	/**
+	 * Serializable para facilitação de tráfego e gravação de arquivo
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id; //Atributos básicos 
+	private String nome;
+	
+	public Categoria() {
+	}
+//Construtor
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
